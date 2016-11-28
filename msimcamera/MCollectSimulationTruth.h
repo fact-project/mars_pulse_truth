b7905719fdeff07ba6b4bf07dc27b535bd294e61
jens buß <jens.buss@tu-dortmund.de>
2016-11-28 18:01:16 +0100

    add containers

--- msimcamera/MCollectSimulationTruth.h
+++ msimcamera/MCollectSimulationTruth.h
@@ -15,28 +15,19 @@ class MAnalogChannels;
 class MCollectSimulationTruth : public MTask
 {
 private:
-    MRawRunHeader    *fRunHeader;  //! Digitization window and frequency
-    MRawEvtHeader    *fEvtHeader;  //! Event header which is filled with the event number
-    MAnalogChannels  *fCamera;     //! Analog channes to be read out
     MParameterD      *fPulsePos;   //! Intended pulse position
     MParameterD      *fTrigger;    //! Position of trigger in the analog signal
-
-    MRawEvtData      *fData;       //! Digitized FADC signal
-
-    Double_t fConversionFactor;    // Conversion factor (arbitrary) from analog signal to FADC counts
+    MPhotonEvent     *fEvt;
+    MPhotonStatistics *fStat;
 
     // MTask
     Int_t  PreProcess(MParList *pList);
     Int_t  Process();
-    Bool_t ReInit(MParList *pList);
-
-    // MParContainer
-    Int_t ReadEnv(const TEnv &env, TString prefix, Bool_t print=kFALSE);
 
 public:
-    MSimReadout(const char *name=NULL, const char *title=NULL);
+    MCollectSimulationTruth(const char *name=NULL, const char *title=NULL);
 
-    ClassDef(MSimReadout, 0) // Task to simulate the analog readout (FADCs)
+    ClassDef(MCollectSimulationTruth, 0) // Task to simulate the analog readout (FADCs)
 };
 
 #endif
